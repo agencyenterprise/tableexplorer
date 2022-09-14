9d3bd676a3d06922979a894c43c66bb0f3afebe2
Lorenzo Tomaz <lolo.tomaz@gmail.com>
2022-09-14 07:14:00 -0300

    adding methods for parsing delete, update and insert actions

--- src/utils/TableTypes.js
+++ src/utils/TableTypes.js
@@ -1,5 +1,5 @@
 export const TABLE_TYPES = ["text", "integer", "blob", "any"];
-
+export const TABLE_CONSTRAINTS = ["PRIMARY KEY", "NOT NULL", "UNIQUE"];
 export function inputTypeByType(type) {
   switch (type) {
     case "text":
@@ -13,4 +13,34 @@ export function inputTypeByType(type) {
     default:
       return "text";
   }
+}
+
+export const parseValues = (column) => {
+  let value = null;
+  switch (column.type) {
+    case "integer":
+      value = parseInt(column.value);
+      break;
+    default:
+      value = `'${column.value}'`;
+  }
+  return value;
+}
+
+export const parseInsertData = (columns, tableName) => {
+  const values = `(${columns.map((column) => parseValues(column)).join(", ")})`;
+  const cols = columns.map((column) => column.name).join(", ");
+  const insertTemplate = `INSERT INTO ${tableName} (${cols}) VALUES ${values};`;
+  return insertTemplate;
+}
+
+export const parseUpdateData = (columns, tableName, id) => {
+  const values = `${columns.map((column) => `${column.name} = ${parseValues(column)}`).join(", ")}`;
+  const updateTemplate = `UPDATE ${tableName} SET ${values}  WHERE id = ${id};`;
+  return updateTemplate;
+}
+
+export const parseDeleteData = (tableName, id) => {
+ const deleteTemplate = `DELETE FROM ${tableName} WHERE id = ${id};`
+ return deleteTemplate
 }
