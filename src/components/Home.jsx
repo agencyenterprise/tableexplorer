eb8565e351eec235ac07356a7a7c9a500a8a8c78
Lorenzo Tomaz <lolo.tomaz@gmail.com>
2022-09-15 04:03:34 -0300

    added home icon

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const HomeIcon = ({ width, height }) => {
+  return (
+    <svg height={height} width={width} viewBox="0 0 24 24">
+      <path fill="currentColor" d="M12 5.69L17 10.19V18H15V12H9V18H7V10.19L12 5.69M12 3L2 12H5V20H11V14H13V20H19V12H22" />
+    </svg>
+  );
+};
+
+export default HomeIcon;
