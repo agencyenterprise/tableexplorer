8e426f4bdb4d8186e5e2c303b9a1911a3e5d87d3
Lorenzo Tomaz <lolo.tomaz@gmail.com>
2022-09-15 04:07:51 -0300

    added more sql parsing methods

--- src/utils/SQLParser.js
+++ src/utils/SQLParser.js
@@ -1,11 +1,13 @@
 // const columns = [{ type: "integer", name: "id", constraints: [] }];
 
+import { TABLE_TYPES } from "./TableTypes";
+
 export function parseCreateTable(columns) {
   let query = "";
 
   const cols = columns.map(
     (column) =>
-      `${column.name} ${column.type} ${column.constraints
+      `"${column.name}" ${column.type} ${column.constraints
         .map((c) => c.toLocaleLowerCase())
         .join(" ")}`
   );
@@ -34,19 +36,18 @@ export const parseInsertData = (columns, tableName) => {
   return insertTemplate;
 }
 
-export const parseUpdateData = (columns, tableName, id) => {
+export const parseUpdateData = (columns, tableName, id, idColumn) => {
   const values = `${columns.map((column) => `${column.name} = ${parseValues(column)}`).join(", ")}`;
-  const updateTemplate = `UPDATE ${tableName} SET ${values}  WHERE id = ${id};`;
+  const updateTemplate = `UPDATE ${tableName} SET ${values}  WHERE ${idColumn} = ${id};`;
   return updateTemplate;
 }
 
-export const parseDeleteData = (tableName, id) => {
- const deleteTemplate = `DELETE FROM ${tableName} WHERE id = ${id};`
+export const parseDeleteData = (tableName, id, idColumn) => {
+ const deleteTemplate = `DELETE FROM ${tableName} WHERE ${idColumn} = ${id};`
  return deleteTemplate
 }
 
 export const hasPK = (columns, tableName) => {
-  console.log(columns)
   const pkFields = columns.filter(v => Object.values(v.constraints).indexOf("PRIMARY KEY") != -1)
   if (pkFields.length > 1){
     throw new Error(`Table ${tableName} must have only one primary key`)
@@ -55,4 +56,20 @@ export const hasPK = (columns, tableName) => {
     throw new Error(`Table '${tableName}' must have a primary key`);
   }
   return pkFields[0]
+}
+
+export const getPKColumn = (columns, tableName) => {
+  const pkFields = (columns||[]).find(v => Object.values(v.constraints).indexOf("PRIMARY KEY") != -1)
+  if (!pkFields) {
+    throw new Error(`Table '${tableName}' must have a primary key`);
+  }
+  return pkFields.name
+}
+
+export const hasColumnTypeAsColumnName = (columns) => {
+  const table_types = TABLE_TYPES.map(v => v.toLocaleLowerCase())
+  const hasColumnTypeAsName = (columns||[]).find(v => table_types.indexOf(v.name.toLocaleLowerCase()) != -1)
+  if (hasColumnTypeAsName) {
+    throw new Error(`Column ${hasColumnTypeAsName.name} has an invalid column name. Column names should not be equal to column types`)
+  }
 }
