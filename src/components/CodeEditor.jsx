e136bd2a523e45a0851d6e8f03e65da50a0d1525
Roger <rogerio@ae.studio>
2022-09-16 15:36:47 -0300

    test with url-loader

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,16 +1,37 @@
 import Nullstack from "nullstack";
 
+// self.MonacoEnvironment = {
+//   getWorkerUrl: function (_moduleId, label) {
+//     if (label === "json") {
+//       return "./json.worker.bundle.js";
+//     }
+//     if (label === "css" || label === "scss" || label === "less") {
+//       return "./css.worker.bundle.js";
+//     }
+//     if (label === "html" || label === "handlebars" || label === "razor") {
+//       return "./html.worker.bundle.js";
+//     }
+//     if (label === "typescript" || label === "javascript") {
+//       return "./ts.worker.bundle.js";
+//     }
+//     return "./editor.worker.bundle.js";
+//   },
+// };
+
 class CodeEditor extends Nullstack {
   editor = null;
   hydrate({ value, onchange, disabled }) {
     try {
       const monaco = require("monaco-editor/esm/vs/editor/editor.api");
-      this.editor = monaco.editor.create(document.getElementById("code-editor"), {
-        value: value,
-        language: "sql",
-        theme: "vs-dark",
-        fontSize: "16px",
-      });
+      this.editor = monaco.editor.create(
+        document.getElementById("code-editor"),
+        {
+          value: value,
+          language: "sql",
+          theme: "vs-dark",
+          fontSize: "16px",
+        }
+      );
       this.editor.updateOptions({ readOnly: !!disabled });
       this.editor.onDidChangeModelContent((_) => {
         onchange && onchange({ query: this.editor.getValue() });
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const [server, client] = require("nullstack/webpack.config");
 const webpack = require("webpack");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const path = require("path");
 
 function customServer(...args) {
   const config = server(...args);
 
-  if (config.mode === 'production') {
+  if (config.mode === "production") {
     config.plugins.push(
       new CopyWebpackPlugin({
-        patterns: ['./src/schema.prisma'],
-      }),
+        patterns: ["./src/schema.prisma"],
+      })
     );
 
-    config.externals = ['@prisma/client'];
+    config.externals = ["@prisma/client"];
   }
 
   return config;
@@ -21,7 +23,7 @@ function customServer(...args) {
 
 function customClient(...args) {
   const config = client(...args);
-  
+
   config.resolve.fallback = {
     ...config.resolve.fallback,
     buffer: require.resolve("buffer/"),
@@ -31,7 +33,27 @@ function customClient(...args) {
       Buffer: ["buffer", "Buffer"],
     })
   );
-  config.plugins.push(new MonacoWebpackPlugin({languages: ["sql"]}));
+  config.plugins.push(new MonacoWebpackPlugin({ languages: ["sql"] }));
+
+  config.module.rules.push({
+    test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+    use: {
+      loader: "url-loader",
+      options: {
+        limit: 100000,
+        name: "[name].[ext]",
+      },
+    },
+  });
+
+  // config.entry = {
+  //   ...config.entry,
+  //   "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+  //   "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
+  //   "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
+  //   "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
+  //   "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker",
+  // }
 
   return config;
 }
