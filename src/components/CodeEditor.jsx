19f0f889ffec5fb49e545fc5616a3c75ed617fda
Lorenzo Tomaz <lolo.tomaz@gmail.com>
2022-09-16 09:15:06 -0300

    added code editor

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,17 +1,36 @@
 import Nullstack from "nullstack";
 
 class CodeEditor extends Nullstack {
-  hydrate() {
-    const monaco = require("monaco-editor/esm/vs/editor/editor.api");
-    monaco.editor.create(document.getElementById("code-editor"), {
-      value: `id integer, name text, primary key (id)`,
-      language: "sql",
-      theme: "vs-dark",
-    });
+  editor = null;
+  hydrate({ value, onchange, disabled, instances }) {
+    try {
+      const monaco = require("monaco-editor/esm/vs/editor/editor.api");
+      this.editor = monaco.editor.create(document.getElementById("code-editor"), {
+        value: value,
+        language: "sql",
+        theme: "vs-dark",
+        fontSize: "16px",
+      });
+      this.editor.updateOptions({ readOnly: !!disabled });
+      this.editor.onDidChangeModelContent((_) => {
+        onchange && onchange({ query: this.editor.getValue() });
+      });
+    } catch (err) {
+      instances.toast._showErrorToast(err.message);
+    }
   }
-
+  getEditorValue() {
+    return this.editor.getValue();
+  }
+  setEditorValue({ query }) {
+    this.editor.setValue(query);
+  }
   render() {
-    return <div class="h-40" id="code-editor" />;
+    return (
+      <div class="border-solid border border-slate-300 h-40">
+        <div class="h-32" id="code-editor" />
+      </div>
+    );
   }
 }
 
