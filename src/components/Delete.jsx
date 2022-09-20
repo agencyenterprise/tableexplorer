80f4d86aeb705914e33846cf46fa53c007841497
LorenzoTomaz <lolo.tomaz@gmail.com>
2022-09-20 08:44:47 -0300

    added customizable width and heigh

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,6 +1,6 @@
-const DeleteIcon = () => {
+const DeleteIcon = ({width, height}) => {
   return (
-    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <svg width={width || 24} height={height || 24} viewBox="0 0 24 24">
       <path fill="currentColor" d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
     </svg>
   );
