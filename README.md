1321f04aab545f4adc228099e6e1392ec2be789e
Roger <rogerio@ae.studio>
2022-09-21 11:44:20 -0300

    removing prisma and adding indexedDB

--- README.md
+++ README.md
@@ -8,10 +8,6 @@ Install the dependencies:
 
 `npm install`
 
-To run in dev mode, you must initialize a database, you can easily do it by:
-
-`npx prisma migrate dev --name init`
-
 Copy the environment sample to a .env file
 
 ```sh
--- server.ts
+++ server.ts
@@ -1,4 +1,3 @@
-import { PrismaClient } from "@prisma/client";
 import Nullstack from "nullstack";
 import Application from "./src/Application";
 import { CustomServerContext } from "./src/types/CustomContexts";
@@ -7,8 +6,6 @@ const context = Nullstack.start(Application) as CustomServerContext;
 
 context.start = async function start() {
   // https://nullstack.app/application-startup
-  const prisma = new PrismaClient();
-  context.prisma = prisma;
 };
 
 export default context;
