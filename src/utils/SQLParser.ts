57502a735c411074fb485869bdf2bfd62c022f4e
Lorenzo Tomaz <lolo.tomaz@gmail.com>
2022-09-25 00:44:48 -0300

    added ast sql parsing and query autocompletion

--- src/utils/SQLParser.ts
+++ src/utils/SQLParser.ts
@@ -2,18 +2,12 @@ import { SchemaColumns } from "@tableland/sdk";
 import { TABLE_TYPES } from "./TableTypes";
 import { Column } from "../types/columns";
 import { Parser } from "node-sql-parser";
-import {AggrFunc, ColumnRef} from "node-sql-parser/types"
-const parser = new Parser();
+import { ColumnRef, Select, Column as SQLColumn, AST } from "node-sql-parser/types";
 
 export function parseCreateTable(columns: Column[]) {
   let query = "";
 
-  const cols = columns.map(
-    (column) =>
-      `"${column.name}" ${column.type} ${column.constraints
-        .map((c) => c.toLocaleLowerCase())
-        .join(" ")}`
-  );
+  const cols = columns.map((column) => `"${column.name}" ${column.type} ${column.constraints.map((c) => c.toLocaleLowerCase()).join(" ")}`);
 
   query += cols.join(", ");
 
@@ -55,36 +49,20 @@ export const parseInsertData = (columns: Column[], tableName: string) => {
   return insertTemplate;
 };
 
-export const parseUpdateData = (
-  columns: Column[],
-  tableName: string,
-  id: number,
-  idColumn: string
-) => {
-  const values = `${columns
-    .map((column) => `${column.name} = ${parseValues(column)}`)
-    .join(", ")}`;
+export const parseUpdateData = (columns: Column[], tableName: string, id: number, idColumn: string) => {
+  const values = `${columns.map((column) => `${column.name} = ${parseValues(column)}`).join(", ")}`;
   const idField = columns.find((col) => col.name == idColumn);
-  const updateTemplate = `UPDATE ${tableName} SET ${values}  WHERE ${idColumn} = ${parseId(
-    idField!,
-    id
-  )};`;
+  const updateTemplate = `UPDATE ${tableName} SET ${values}  WHERE ${idColumn} = ${parseId(idField!, id)};`;
   return updateTemplate;
 };
 
-export const parseDeleteData = (
-  tableName: string,
-  id: number,
-  idColumn: string
-) => {
+export const parseDeleteData = (tableName: string, id: number, idColumn: string) => {
   const deleteTemplate = `DELETE FROM ${tableName} WHERE ${idColumn} = ${id};`;
   return deleteTemplate;
 };
 
 export const hasPK = (columns: Column[], tableName: string) => {
-  const pkFields = columns.filter(
-    (v) => Object.values(v.constraints).indexOf("PRIMARY KEY") != -1
-  );
+  const pkFields = columns.filter((v) => Object.values(v.constraints).indexOf("PRIMARY KEY") != -1);
   if (pkFields.length > 1) {
     throw new Error(`Table ${tableName} must have only one primary key`);
   }
@@ -95,9 +73,7 @@ export const hasPK = (columns: Column[], tableName: string) => {
 };
 
 export const getPKColumn = (columns: SchemaColumns, tableName: string) => {
-  const pkFields = (columns || []).find(
-    (v) => (v.constraints || []).indexOf("PRIMARY KEY") != -1
-  );
+  const pkFields = (columns || []).find((v) => (v.constraints || []).indexOf("PRIMARY KEY") != -1);
   if (!pkFields) {
     throw new Error(`Table '${tableName}' must have a primary key`);
   }
@@ -105,34 +81,23 @@ export const getPKColumn = (columns: SchemaColumns, tableName: string) => {
 };
 
 export const getPKColumnIndex = (columns: SchemaColumns) => {
-  const index = (columns || [])
-    .map((v) => (v.constraints || []).indexOf("PRIMARY KEY") != -1)
-    .indexOf(true);
+  const index = (columns || []).map((v) => (v.constraints || []).indexOf("PRIMARY KEY") != -1).indexOf(true);
   return index;
 };
 
 export const hasColumnTypeAsColumnName = (columns: Column[]) => {
   const table_types = TABLE_TYPES.map((v) => v.toLocaleLowerCase());
-  const hasColumnTypeAsName = (columns || []).find(
-    (v) => table_types.indexOf(v.name.toLocaleLowerCase()) != -1
-  );
+  const hasColumnTypeAsName = (columns || []).find((v) => table_types.indexOf(v.name.toLocaleLowerCase()) != -1);
   if (hasColumnTypeAsName) {
-    throw new Error(
-      `Column ${hasColumnTypeAsName.name} has an invalid column name. Column names should not be equal to column types`
-    );
+    throw new Error(`Column ${hasColumnTypeAsName.name} has an invalid column name. Column names should not be equal to column types`);
   }
 };
 export const parseTableName = (chainId: number, tableName: string) => {
-  return [
-    (tableName || "").replace(
-      new RegExp(`_[${chainId}]+[0-9A-Za-z_]+`, "g"),
-      ""
-    ),
-  ].reduce((acc, v) => (v ? v : acc), tableName);
+  return [(tableName || "").replace(new RegExp(`_[${chainId}]+[0-9A-Za-z_]+`, "g"), "")].reduce((acc, v) => (v ? v : acc), tableName);
 };
 
 export const isReadQuery = (query: string) => {
-  return !!((query || "").toLocaleLowerCase().match(/select/) || []).length;
+  return !!((query || "").toLocaleLowerCase().match(/\s*select\s+/) || []).length;
 };
 
 export const countQuery = (columns: SchemaColumns, tableName: string) => {
@@ -146,12 +111,8 @@ export const parseSelectQuery = (query: string) => {
   const LIMIT = "LIMIT";
   const qTrim = query.trim();
   const lastSemiColon = qTrim.lastIndexOf(";");
-  const shouldDeleteLastSemiColon =
-    lastSemiColon == qTrim.length - 1 ? ";" : "";
-  const keywords = [
-    ...[OFFSET, LIMIT].map((v) => v),
-    ...[OFFSET, LIMIT].map((v) => v.toLocaleLowerCase()),
-  ];
+  const shouldDeleteLastSemiColon = lastSemiColon == qTrim.length - 1 ? ";" : "";
+  const keywords = [...[OFFSET, LIMIT].map((v) => v), ...[OFFSET, LIMIT].map((v) => v.toLocaleLowerCase())];
   const removeOffsetLimit = () =>
     keywords.reduce((acc, v) => {
       acc = acc.replace(new RegExp(`${v}+\\s*[0-9]+`), "");
@@ -160,22 +121,29 @@ export const parseSelectQuery = (query: string) => {
   return removeOffsetLimit().replace(shouldDeleteLastSemiColon, "").trim();
 };
 
-export const buildSelectQuery = (
-  query: string,
-  limit: number = 10,
-  offset: number = 0
-) => {
+export const buildSelectQuery = (query: string, limit: number = 5, offset: number = 0) => {
+  console.log("buildSelectQuery");
+  console.log(limit, offset);
   if (!isReadQuery(query)) {
+    console.log("NO");
     return query;
   }
-  if (!rawRecords(query)) {
-    return query;
-  }
-  const parsedQuery = parseSelectQuery(query);
-  const hasCountQuery = parsedQuery.match(/count\s*\(/g);
-  return hasCountQuery
-    ? parsedQuery
-    : parsedQuery + ` LIMIT ${limit} OFFSET ${offset};`;
+  const parser = new Parser();
+  const ast: AST | AST[] = parser.astify(query, { database: "sqlite" }) as Select;
+  const limit_offset = {
+    seperator: "offset",
+    value: [
+      { type: "number", value: limit },
+      { type: "number", value: offset },
+    ],
+  };
+  ast.limit = limit_offset;
+  console.log(ast);
+  const sql = removeReplaceStatements(parser.sqlify(ast));
+  console.log(sql);
+  const originalSQLHasJoin = !!removeReplaceStatements(query).match(/\s+JOIN\s+/g);
+  const newSqlHasInnerJoin = !!sql.match(/\s+INNER\s+JOIN\s+/g);
+  return originalSQLHasJoin && newSqlHasInnerJoin ? removeReplaceStatements(sql, true) : sql;
 };
 
 export const hasCountStatement = (query: string) => {
@@ -201,21 +169,63 @@ export const rawRecords = (query: string) => {
   }, true);
 };
 
+export const removeReplaceStatements = (query: string, simpleJoin: boolean = false) => {
+  let keywords = [
+    { pattern: /COUNT\(/g, value: "count(" },
+    { pattern: /AVG\(/g, value: "avg(" },
+    { pattern: /SUM\(/g, value: "sum(" },
+    { pattern: /MIN\(/g, value: "min(" },
+    { pattern: /MAX\(/g, value: "max(" },
+    { pattern: /\s+join\s+/g, value: " JOIN " },
+  ];
+  keywords = simpleJoin ? [...keywords, { pattern: /\s+INNER\s+JOIN\s+/g, value: " JOIN " }] : keywords;
+  return keywords.reduce((acc: string, v: { pattern: RegExp; value: string }) => {
+    acc = acc.replace(v.pattern, v.value);
+    return acc;
+  }, query);
+};
+
+export const isInsertRecord = (query: string) => {
+  return !!query.match(/INSERT\s*/g);
+};
+
+export const isUpdateRecord = (query: string) => {
+  return !!query.match(/UPDATE\s*/g);
+};
+
+export const isAggregatorOnly = (query: string) => {
+  const parser = new Parser();
+  const ast: AST | AST[] = parser.astify(query, { database: "sqlite" }) as Select;
+  const hasColumnRef =
+    ast.columns == "*"
+      ? false
+      : (Array.isArray(ast.columns) ? ast.columns : []).reduce((acc: boolean, v: SQLColumn) => {
+          acc = v.expr.type == "aggr_func" && acc;
+          return acc;
+        }, true);
+  return hasColumnRef;
+};
+
 export const parseCountQuery = (query: string) => {
-  const ast: Select = parser.astify(
-    "SELECT q.id, q.name, j.skill FROM query_table_1 as q JOIN join_table_2 as j WHERE q.id = j.id"
-  ) as Select;
-  const colRefexpr = { type: 'column_ref', table: 'q', column: 'id' } as ColumnRef
-  const ref = {
-    type: 'aggr_func',
-  name: "count",
-  args: colRefexpr,
-  }
-  //ast.columns = [newColumn]
-  /* const limit_offset = {
-      seperator: 'offset',
-      value: [ { type: 'number', value: 10 }, { type: 'number', value: 0 } ]
-    }
-  ast.limit = limit_offset
-  console.log(parser.sqlify(ast)) */
+  const parser = new Parser();
+  const ast: AST | AST[] = parser.astify(query, { database: "sqlite" }) as Select;
+  const colRefexpr = ((Array.isArray(ast.columns) ? ast.columns : []).find((v: SQLColumn) => v.expr.type == "column_ref") as ColumnRef) || {
+    expr: {
+      type: "star",
+      value: "*",
+    },
+  };
+  const countAst = {
+    expr: {
+      type: "aggr_func",
+      name: "COUNT",
+      args: colRefexpr,
+    },
+  } as SQLColumn;
+  ast.columns = [countAst];
+  ast.limit = null;
+  const sql = removeReplaceStatements(parser.sqlify(ast));
+  const originalSQLHasJoin = !!removeReplaceStatements(query).match(/\s+JOIN\s+/g);
+  const newSqlHasInnerJoin = !!sql.match(/\s+INNER\s+JOIN\s+/g);
+  return originalSQLHasJoin && newSqlHasInnerJoin ? removeReplaceStatements(sql, true) : sql;
 };
